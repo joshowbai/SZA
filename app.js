154e011928dc093d4e86490d087b82d62e798902
joshowbai <jbailey3@aggies.ncat.edu>
2017-07-12 10:34:11 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cfenv = require("cfenv");
+var index = require('./routes/index');
+var about = require('./routes/about');
+var login = require('./routes/login');
+
+var app = express();
+
+app.locals.points = "333,333";
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var mydb;
+
+app.post("/api/visitors", function (request, response) {
+    var userName = request.body.name;
+    if(!mydb) {
+        console.log("No database.");
+        response.send("Hello " + userName + "!");
+        return;
+    }
+    // insert the username as a document
+    mydb.insert({ "name" : userName }, function(err, body, header) {
+        if (err) {
+            return console.log('[mydb.insert] ', err.message);
+        }
+        response.send("Hello " + userName + "! I added you to the database.");
+    });
+});
+
+
+//pages
+app.use('/login', login);
+app.use('/', index);
+app.use('/about', about);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
